1196b01c7cc2cef481b6c66db3b69d3bece6d18a
jasmin47 <alxsanborn@gmail.com>
2016-09-06 09:32:50 -0400

    done

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,1 +1,14 @@
-// Add your JavaScript here
+// // Add your JavaScript here
+$(function(){
+  addHeartEventHandler();
+});
+//
+function addHeartEventHandler(){
+   $('.like_button').on('ajax:complete', function(e, request, status){
+     $(this).find('span').toggleClass('red-heart grey-heart');
+     this.nextElementSibling.innerHTML = request.responseJSON.message;
+
+   });
+
+
+}
